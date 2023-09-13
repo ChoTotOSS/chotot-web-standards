d5f09714c03f04f92b7c1c3cfda0d007ff4a0231
tunguyen-ct <tunguyen@chotot.vn>
2023-09-13 10:44:45 +0700

    feat: port no-nested-ternary

--- packages/eslint-plugin-chotot/src/rules/no-nested-ternary.ts
+++ packages/eslint-plugin-chotot/src/rules/no-nested-ternary.ts
@@ -1,13 +1,12 @@
 import { defineRule } from '../utils/define-rule';
+import { isParenthesized } from '@eslint-community/eslint-utils';
 
 const MESSAGE_ID_TOO_DEEP = 'too-deep';
+const MESSAGE_ID_SHOULD_PARENTHESIZED = 'should-parenthesized';
 const messages = {
-  [MESSAGE_ID_TOO_DEEP]: 'Do not nest ternary expressions more than one level.',
+  [MESSAGE_ID_TOO_DEEP]: 'Do not nest ternary expressions.',
+  [MESSAGE_ID_SHOULD_PARENTHESIZED]: 'Nest ternary expression should be parenthesized.',
 };
-
-const nestTernarySelector = (level) =>
-  `:not(ConditionalExpression)${' > ConditionalExpression'.repeat(level)}`;
-
 
 //------------------------------------------------------------------------------
 // Rule Definition
@@ -16,15 +15,41 @@ export = defineRule({
   meta: {
     type: 'suggestion',
     docs: {
-      description: 'Disallow more-than-one nested ternary expressions.',
+      description: 'Disallow nested ternary expressions.',
     },
+    fixable: 'code',
     messages,
   },
   create(context) {
     return {
-      [nestTernarySelector(3)]: (node) => {
-        // Nesting more than one level not allowed.
-        return context.report({ node, messageId: MESSAGE_ID_TOO_DEEP });
+      ConditionalExpression(node) {
+        if (
+          [node.test, node.consequent, node.alternate].some(
+            (node) => node.type === 'ConditionalExpression'
+          )
+        ) {
+          return;
+        }
+
+        const { sourceCode } = context;
+        const ancestors = sourceCode.getAncestors(node).reverse();
+        const nestLevel = ancestors.findIndex((node) => node.type !== 'ConditionalExpression');
+
+        if (nestLevel === 1 && !isParenthesized(node, sourceCode)) {
+          return context.repot({
+            node,
+            messageId: MESSAGE_ID_SHOULD_PARENTHESIZED,
+            fix: (fixer) => [fixer.insertTextBefore(node, '('), fixer.insertTextAfter(node, ')')],
+          });
+        }
+
+        // Nesting more than one level not allowed
+        if (nestLevel > 1) {
+          return context.report({
+            node: nestLevel > 2 ? ancestors[nestLevel - 3] : node,
+            messageId: MESSAGE_ID_TOO_DEEP,
+          });
+        }
       },
     };
   },
